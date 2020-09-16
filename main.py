fc6788cc30bd9e40185d43229fc175ef03f9fa7d
wenjinzhang <44008219+wenjinzhang@users.noreply.github.com>
2020-09-15 23:09:07 -0400

    updata

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from flask import Flask, jsonify, request
-from util import cast_command
+from flask import Flask, jsonify, request, json
+import os
 app = Flask(__name__)
 
 
-@app.route('/acclog/')
+@app.route('/acclog', methods=['GET', 'POST'])
 def acclog():
-    print("request.data: %s"%request.data)
+    acclog =  json.loads(request.data.decode('utf-8'))
+    file_name = acclog['file_name'].replace("/", "_")
+    if not os.path.exists("data"):
+        os.mkdir("data")
+
+    path="{}/{}.csv".format("./data", file_name)
+    writer = open(path, 'w')
+    for line in acclog['data']:
+        writer.write(line+"\n")
+
+    writer.close()
     return jsonify(msg="success to upload server")
 
 
+@app.route('/')
+def hello_world():
+    return 'Voice Service is Running!'
+
+
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
